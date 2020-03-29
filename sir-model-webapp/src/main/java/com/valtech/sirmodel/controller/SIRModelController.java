2a700bdebf2ee71c8a27b45c1d4e2c04877a03a1
Antonio Troiano <antonio.troiano@valtech.com>
2020-03-29 22:24:43 +0200

    Spring and thymeleaf added

--- sir-model-webapp/src/main/java/com/valtech/sirmodel/controller/SIRModelController.java
+++ sir-model-webapp/src/main/java/com/valtech/sirmodel/controller/SIRModelController.java
@@ -1,6 +1,67 @@
 package com.valtech.sirmodel.controller;
 
+import com.valtech.sirmodel.model.ValuesSIR;
+import com.valtech.sirmodel.service.Derivative;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("sir-model")
 public class SIRModelController {
 
+    Derivative derivative = new Derivative();
+    List<Double> resultSusceptible;
+    List<Double> resultInfected;
+    List<Double> resultRecovered;
 
+    @GetMapping
+    public String showSIRModel(Model model, ValuesSIR valuesSIR) {
+
+        resultSusceptible =
+                derivative.calculation(45400, 2100, 2500, 0.00001, 0.07142857143,
+                        30).get("Susceptible");
+        resultInfected =
+                derivative.calculation(45400, 2100, 2500, 0.00001, 0.07142857143,
+                        30).get("Infected");
+        resultRecovered =
+                derivative.calculation(45400, 2100, 2500, 0.00001, 0.07142857143,
+                        30).get("Recovered");
+
+        model.addAttribute("resultSusceptible", resultSusceptible);
+        model.addAttribute("resultInfected", resultInfected);
+        model.addAttribute("resultRecovered", resultRecovered);
+
+        return "sir-model";
+    }
+
+    @PostMapping("/newInput")
+    public String postDataToChar(@ModelAttribute("valuesSIR") ValuesSIR valuesSIR, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+            return "sir-model";
+        }
+
+        resultSusceptible =
+                derivative.calculation(45400, 2100, 2500, valuesSIR.getTransmissionRate(),
+                        0.07142857143, 30).get("Susceptible");
+        resultInfected =
+                derivative.calculation(45400, 2100, 2500, valuesSIR.getTransmissionRate(),
+                        0.07142857143, 30).get("Infected");
+        resultRecovered =
+                derivative.calculation(45400, 2100, 2500, valuesSIR.getTransmissionRate(),
+                        0.07142857143, 30).get("Recovered");
+
+        model.addAttribute("resultSusceptible", resultSusceptible);
+        model.addAttribute("resultInfected", resultInfected);
+        model.addAttribute("resultRecovered", resultRecovered);
+
+        return showSIRModel(model, valuesSIR);
+    }
 }
--- sir-model-webapp/src/main/java/com/valtech/sirmodel/model/ValuesSIR.java
+++ sir-model-webapp/src/main/java/com/valtech/sirmodel/model/ValuesSIR.java
@@ -1,4 +1,36 @@
-package com.valtech.sirmodel;
+package com.valtech.sirmodel.model;
 
 public class ValuesSIR {
+
+    Double transmissionRate;
+    Double recoveryRate;
+
+    public ValuesSIR(Double transmissionRate, Double recoveryRate) {
+        this.transmissionRate = transmissionRate;
+        this.recoveryRate = recoveryRate;
+    }
+
+    public Double getTransmissionRate() {
+        return transmissionRate;
+    }
+
+    public void setTransmissionRate(Double transmissionRate) {
+        this.transmissionRate = transmissionRate;
+    }
+
+    public Double getRecoveryRate() {
+        return recoveryRate;
+    }
+
+    public void setRecoveryRate(Double recoveryRate) {
+        this.recoveryRate = recoveryRate;
+    }
+
+    @Override
+    public String toString() {
+        return "ValuesSIR{" +
+                "transmissionRate=" + transmissionRate +
+                ", recoveryRate=" + recoveryRate +
+                '}';
+    }
 }
