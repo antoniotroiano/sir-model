57d89eff32b19d9ef0ec23021035c0d50c4a872c
Antonio Troiano <antonio.troiano@valtech.com>
2020-03-29 23:47:59 +0200

    Controller test

---
+++ sir-model-webapp/src/test/java/com/valtech/sirmodel/controller/SIRModelControllerTest.java
@@ -0,0 +1,70 @@
+package com.valtech.sirmodel.controller;
+
+import com.valtech.sirmodel.model.ValuesSIR;
+import com.valtech.sirmodel.service.Derivative;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SIRModelControllerTest {
+
+    private Map<String, List<Double>> map = new HashMap<>();
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private Derivative derivative;
+
+    @InjectMocks
+    private SIRModelController sirModelController;
+
+    @BeforeEach
+    public void setUp() {
+
+        List<Double> susList = new ArrayList<>();
+        susList.add(1.0);
+
+        List<Double> infList = new ArrayList<>();
+        infList.add(1.0);
+
+        List<Double> reList = new ArrayList<>();
+        infList.add(1.0);
+
+        map.put("Susceptible", susList);
+        map.put("Infected", infList);
+        map.put("Recovered", reList);
+    }
+
+    @Test
+    public void showShowModel() throws Exception {
+        when(derivative.calculation(anyDouble(), anyDouble(), anyDouble(), anyDouble(), anyDouble(), anyInt())).thenReturn(map);
+
+        mockMvc.perform(get("/sir-model"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Transmission rate:")))
+                .andExpect(content().string(containsString("Recovery rate:")));
+    }
+}
